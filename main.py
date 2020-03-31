f6c1f7d6a9e73b8dc33c07caad6948dffcecb55e
Vinicius Lima <vinicius.gl098@gmail.com>
2020-03-31 18:25:41 -0300

    Arvore AST e entrada com extensao .php, Casting fix, Evaluate on tests
    * Arvore AST e entrada com extensao .php
    
    * Casting fix, Evaluate on tests

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from Parser import Parser
 if __name__ == "__main__":
 
     eq = sys.argv[1]
-    resultado = Parser.run(eq)
-    # resultado = Parser.run('(2+2)/2')
+    
+    if(".php" in eq):
+        f = open(eq)
+        code = "".join([x.strip() for x in f.readlines()]) 
+        resultado = Parser.run(code)
+        resultado = resultado.Evaluate()
+        print(resultado)
+    else:
+        raise Exception("ERRO")
+    
 
-    print(resultado)
